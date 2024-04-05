b75a556c98e5f3999b04cbdf2597b5418f662d4f
0xJayPi <jpcampaya@gmail.com>
2024-04-05 13:03:21 +0200

    assignment 3.3 block, jumping to 3.4

--- test/ERC4626.t.sol
+++ test/ERC4626.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console, StdCheats} from "forge-std/Test.sol";
 import {ERC4626} from "../src/tokens/ERC4626.sol";
 import {ERC20} from "../src/tokens/ERC20.sol";
 import {ERC20Mock} from "../src/mocks/ERC20Mock.sol";
@@ -24,7 +24,7 @@ contract ERC4626Test is Test, KontrolCheats {
         asset = new ERC20Mock();
         vault = new ERC4626(ERC20(address(asset)), "Vault", "VAULT");
 
-        kevm.symbolicStorage(address(vault));
+        // kevm.symbolicStorage(address(vault));
     }
 
     // totalAssets MUST NOT revert
@@ -82,4 +82,56 @@ contract ERC4626Test is Test, KontrolCheats {
         vm.startPrank(caller);
         assert(asset.decimals() <= vault.decimals());
     }
+
+    // Fuzzing OK
+    // @todo Symbolic
+    function test_convertToShares_reverts(address from, uint128 amount) public {
+        _notBuiltinAddress(from);
+
+        ERC20Mock(address(asset)).mint(address(this), 1);
+        asset.approve(address(vault), 1);
+        vault.deposit(1, address(this));
+
+        vault.pause();
+
+        vm.prank(from);
+        vm.expectRevert(bytes4(keccak256("EnforcedPause()")));
+        vault.convertToShares(amount);
+    }
+
+    // make `from` and `amount` symbolic - DONE
+    // assume `from` isn’t a built-in address - DONE
+    // assume `vault`’s `totalSupply` is positive (otherwise, it’ll be symbolic and the execution with branch)
+    // ensure the contract is paused - DONE
+    // ensure `convertToShares` is called by a symbolic `from` - DONE
+    // check that `convertToShares` always reverts - DONE
+
+    // @todo in the assignment, it mentions storage mapping manipulation, review the recording
+    function test_transfer(address from, address to, uint256 amount) public {
+        _notBuiltinAddress(from);
+        _notBuiltinAddress(to);
+        deal(address(vault), from, amount);
+        vm.assume(from != to);
+
+        uint256 fromPrevBalance = vault.balanceOf(from);
+        uint256 toPrevBalance = vault.balanceOf(to);
+
+        vm.prank(from);
+        vault.transfer(to, amount);
+
+        uint256 fromPostBalance = vault.balanceOf(from);
+        uint256 toPostBalance = vault.balanceOf(to);
+
+        assert(fromPostBalance == fromPrevBalance - amount);
+        assert(toPostBalance == toPrevBalance + amount);
+    }
+    // make `from`, `to`,  and `amount` symbolic - DONE
+    // assume `from` and `to` aren’t built-in addresses - DONE
+    // assume `from` has enough `vault` tokens to transfer - DONE
+    // assume `from` and `to` are different addresses - DONE
+    // record `from` and `to` balances pre-`transfer` - DONE
+    // ensure `vault.transfer()` is called by a symbolic `from` with `to` and `amount` as parameters - DONE
+    // record `from` and `to` balances post-`transfer` - DONE
+    // check if the balances have been updated correctly wrt the `amount` transferred - DONE
+    // be wary of overflow checks — for the purposes of this exercise, ignore the possible overflow in `to`’s balance, as discussed with respect to test_assume_overflow
 }
